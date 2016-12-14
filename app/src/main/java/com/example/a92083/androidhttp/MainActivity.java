df1bc3b6cde4f42f6b7f3f45f4895a8fad068a62
hhllnw <920832559@qq.com>
2016-12-14 15:32:04 +0800

    post

--- app/src/main/java/com/example/a92083/androidhttp/MainActivity.java
+++ app/src/main/java/com/example/a92083/androidhttp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.a92083.androidhttp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       postTest();
+    }
+
+    private void postTest(){
+
+
     }
 }
